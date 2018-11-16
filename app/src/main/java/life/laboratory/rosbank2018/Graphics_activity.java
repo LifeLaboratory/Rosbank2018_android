aca5b8ca02f66cf96dddd9c309dd9f8f0f282e67
alexandravg <a.efimova09@gmail.com>
2018-11-17 04:14:19 +0700

    Added extended graphics activity

--- app/src/main/java/life/laboratory/rosbank2018/Graphics_activity.java
+++ app/src/main/java/life/laboratory/rosbank2018/Graphics_activity.java
@@ -8,15 +8,11 @@ import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.GraphViewSeries;
-import com.jjoe64.graphview.GraphView.GraphViewData;
-import com.jjoe64.graphview.LineGraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class Graphics_activity extends AppCompatActivity {
 
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -25,13 +21,10 @@ public class Graphics_activity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -42,8 +35,6 @@ public class Graphics_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphics_activity);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
@@ -61,7 +52,7 @@ public class Graphics_activity extends AppCompatActivity {
                         new GraphViewData(4, 1.0d), new GraphViewData(5, 1.3d) });*/
 
         GraphView graphView;// = new LineGraphView(this, "График каких-то данных");
-        graphView = findViewById(R.id.graphView);
+        graphView = (GraphView) findViewById(R.id.graphView);
         graphView.addSeries(series);
 
         //LinearLayout layout = (LinearLayout) findViewById(R.id.layout);
